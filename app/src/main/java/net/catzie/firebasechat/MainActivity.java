ec5a46d9f6d8dfcbf680d2aa87b0e2298cba76f1
Catzie (Macaron) <i@catzie.net>
2017-03-12 23:51:14 +0800

    Add value for
    SIGN_IN_REQUEST_CODE

--- app/src/main/java/net/catzie/firebasechat/MainActivity.java
+++ app/src/main/java/net/catzie/firebasechat/MainActivity.java
@@ -10,6 +10,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int SIGN_IN_REQUEST_CODE = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
