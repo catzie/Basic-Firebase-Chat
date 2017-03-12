39db14d5f2495a29cae11be71d36780438a62deb
Catzie (Macaron) <i@catzie.net>
2017-03-12 23:49:13 +0800

    Add onActivityResult() in MainActivity

--- app/src/main/java/net/catzie/firebasechat/MainActivity.java
+++ app/src/main/java/net/catzie/firebasechat/MainActivity.java
@@ -1,5 +1,6 @@
 package net.catzie.firebasechat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -40,4 +41,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayChatMessages() {
 
-    }}
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+
+                // Close the app
+                finish();
+            }
+        }
+
+    }
+}
